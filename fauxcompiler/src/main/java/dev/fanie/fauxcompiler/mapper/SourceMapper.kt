e1aef6ebbade7d275cc0e0384cb03c6930009841
iFanie <fanieveizis@gmail.com>
2019-03-03 22:29:03 +0200

    Create mapping from AnnotatedVariable to Property

---
+++ fauxcompiler/src/main/java/dev/fanie/fauxcompiler/mapper/SourceMapper.kt
@@ -0,0 +1,22 @@
+package dev.fanie.fauxcompiler.mapper
+
+import com.izikode.izilib.basekotlincompiler.source.member.field.AnnotatedVariableSource
+import dev.fanie.faux.Faux
+import dev.fanie.fauxcompiler.entity.Property
+
+fun AnnotatedVariableSource<Faux>.toProperty(): Property {
+    val annotation = this.annotation.value
+
+    return Property(
+        this.parent.info.name,
+        annotation[1],
+        this.info.name,
+        annotation[0]
+    )
+}
+
+fun ArrayList<AnnotatedVariableSource<Faux>>.toProperties() = mutableListOf<Property>().apply {
+    this@toProperties.forEach { source ->
+        add(source.toProperty())
+    }
+}
