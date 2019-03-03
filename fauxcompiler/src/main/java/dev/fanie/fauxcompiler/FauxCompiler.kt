c72c1d186621a4938898b35ed84ef2bce1bd5665
iFanie <fanieveizis@gmail.com>
2019-03-03 22:35:57 +0200

    Use model & presenter in compiler round processing

--- fauxcompiler/src/main/java/dev/fanie/fauxcompiler/FauxCompiler.kt
+++ fauxcompiler/src/main/java/dev/fanie/fauxcompiler/FauxCompiler.kt
@@ -2,16 +2,22 @@ package dev.fanie.fauxcompiler
 
 import com.izikode.izilib.basekotlincompiler.BaseKotlinCompiler
 import dev.fanie.faux.Faux
+import dev.fanie.fauxcompiler.component.CompilerModel
+import dev.fanie.fauxcompiler.component.CompilerPresenter
 import kotlin.reflect.KClass
 
 class FauxCompiler : BaseKotlinCompiler() {
+
+    private val presenter: CompilerPresenter by lazy { CompilerPresenter() }
 
     override val processes: Array<KClass<out Any>> = arrayOf(
         Faux::class
     )
 
     override val roundHandler: CompilationRoundHandler.() -> Unit = {
-
+        presenter.process(
+            CompilerModel(this)
+        )
     }
 
     override val finallyHandler: FinallyHandler.() -> Unit = {
